1b2657e1322441504e8dc6f35129e0799b717ed1
Zhoubai <1246000205@qq.com>
2024-06-03 19:47:49 +0800

    show history details

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +14,67 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Clear Shot',
-      home: ImagePickerDemo(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainScreen()),
+      );
+    });
+
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'ClearShot',
+          style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    ImagePickerDemo(),
+    HistoryScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Main',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'History',
+          ),
+        ],
+      ),
     );
   }
 }
@@ -32,8 +94,7 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
   @override
   void initState() {
     super.initState();
-    _urlController.text = 'http://frp-fly.top:57409/upload/'; // Initialize URL text field
-    //http://10.37.36.216:8000/upload/
+    _urlController.text = 'http://8.138.119.19:8000/upload/';
   }
 
   @override
@@ -54,6 +115,7 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
 
   Future<void> _uploadImage() async {
     if (_image == null || _urlController.text.isEmpty) {
+      _showSnackBar('请先选择图片并输入有效的URL');
       return;
     }
 
@@ -69,14 +131,25 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
 
     request.fields['sliderValue'] = _sliderValue.toString();
 
-    var response = await request.send();
-    if (response.statusCode == 200) {
-      http.Response res = await http.Response.fromStream(response);
-      Uint8List responseData = res.bodyBytes;
-      _blendImages(responseData);
-      _showSnackBar('Image uploaded and blended successfully');
-    } else {
-      _showSnackBar('Failed to upload image. Error code: ${response.statusCode}');
+    try {
+      var response = await request.send();
+      if (response.statusCode == 200) {
+        http.Response res = await http.Response.fromStream(response);
+        Uint8List responseData = res.bodyBytes;
+        _blendImages(responseData);
+        await _saveToHistory(responseData);
+        if (mounted) {
+          _showSnackBar('图片上传和混合成功');
+        }
+      } else {
+        if (mounted) {
+          _showSnackBar('图片上传失败. 错误代码: ${response.statusCode}');
+        }
+      }
+    } catch (e) {
+      if (mounted) {
+        _showSnackBar('图片上传过程中发生错误: $e');
+      }
     }
   }
 
@@ -118,6 +191,33 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
     setState(() {
       _blendedImage = blendedImage;
     });
+  }
+
+  Future<void> _saveToHistory(Uint8List responseData) async {
+    if (_image == null || _blendedImage == null) {
+      return;
+    }
+
+    try {
+      final directory = await getTemporaryDirectory();
+      final historyDir = Directory('${directory.path}/history');
+      if (!await historyDir.exists()) {
+        await historyDir.create();
+      }
+      final timestamp = DateFormat('yyyy-MM-dd_HH-mm-ss').format(DateTime.now());
+      final imageDir = Directory('${historyDir.path}/$timestamp');
+      await imageDir.create();
+
+      final originalImageFile = File('${imageDir.path}/original.png');
+      final blendedImageFile = File('${imageDir.path}/blended.png');
+
+      await originalImageFile.writeAsBytes(_image!.readAsBytesSync());
+      await blendedImageFile.writeAsBytes(responseData);
+
+      print('图片已保存到: ${imageDir.path}');
+    } catch (e) {
+      print('保存图片过程中发生错误: $e');
+    }
   }
 
   void _showSnackBar(String message) {
@@ -181,6 +281,179 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
                   : Image.memory(Uint8List.fromList(img.encodePng(_blendedImage!))),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class HistoryScreen extends StatefulWidget {
+  @override
+  _HistoryScreenState createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  List<Directory> _directories = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHistory();
+  }
+
+  Future<void> _loadHistory() async {
+    try {
+      final directory = await getTemporaryDirectory();
+      final historyDir = Directory('${directory.path}/history');
+      if (await historyDir.exists()) {
+        final directories = historyDir
+            .listSync()
+            .where((entity) => entity is Directory)
+            .map((entity) => entity as Directory)
+            .toList()
+          ..sort((a, b) => b.path.compareTo(a.path));
+
+        setState(() {
+          _directories = directories;
+        });
+      }
+    } catch (e) {
+      print('加载历史记录过程中发生错误: $e');
+    }
+  }
+
+  void _openDetailScreen(Directory directory) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailScreen(directory: directory),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('History'),
+      ),
+      body: _directories.isEmpty
+          ? Center(child: Text('No history available.'))
+          : ListView.builder(
+        itemCount: _directories.length,
+        itemBuilder: (context, index) {
+          final directory = _directories[index];
+          final originalImageFile = File('${directory.path}/original.png');
+
+          return FutureBuilder<Uint8List>(
+            future: originalImageFile.readAsBytes(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  return Container(
+                    height: 150, // Increased height for each item
+                    child: ListTile(
+                      leading: Image.memory(snapshot.data!, width: 100, height: 100, fit: BoxFit.cover),
+                      title: Text(directory.path.split('/').last),
+                      onTap: () => _openDetailScreen(directory),
+                    ),
+                  );
+                } else {
+                  return Container(
+                    height: 150, // Increased height for each item
+                    child: ListTile(
+                      leading: Icon(Icons.broken_image),
+                      title: Text('Failed to load image'),
+                    ),
+                  );
+                }
+              } else {
+                return Container(
+                  height: 150, // Increased height for each item
+                  child: ListTile(
+                    leading: CircularProgressIndicator(),
+                    title: Text('Loading...'),
+                  ),
+                );
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatefulWidget {
+  final Directory directory;
+
+  DetailScreen({required this.directory});
+
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  bool _isImageMaximized = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final originalImageFile = File('${widget.directory.path}/original.png');
+    final blendedImageFile = File('${widget.directory.path}/blended.png');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FutureBuilder<Uint8List>(
+              future: originalImageFile.readAsBytes(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasData) {
+                    return Image.memory(snapshot.data!, width: 200, height: 200, fit: BoxFit.cover);
+                  } else {
+                    return Icon(Icons.broken_image, size: 100);
+                  }
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isImageMaximized = !_isImageMaximized;
+                });
+              },
+              child: FutureBuilder<Uint8List>(
+                future: blendedImageFile.readAsBytes(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      return _isImageMaximized
+                          ? Expanded(child: Image.memory(snapshot.data!))
+                          : Image.memory(snapshot.data!, width: 200, height: 200, fit: BoxFit.cover);
+                    } else {
+                      return Icon(Icons.broken_image, size: 100);
+                    }
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
